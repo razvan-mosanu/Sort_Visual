4b4b6ecc70be05dece8ea990d449a01a858ab5d4
Razvan <razvanamosanu@gmail.com>
2026-03-08 13:20:08 +0200

    QuickSort, HeapSort, Amestecare, slider de viteza

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,19 +4,67 @@
 #include <QPainter>
 #include <QCoreApplication>
 #include <QPushButton>
+#include <random>
+#include <QSlider>
 
 MainWindow::MainWindow(size_t elemente, const std::vector<int> &v, QWidget *parent): QMainWindow(parent) , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
     this->elemente = elemente;
     this->v = v;
-    QPushButton *btnBubble = new QPushButton("Start Bubble Sort", this);
-    QPushButton *btnMergeSort = new QPushButton("Start Merge Sort", this);
+    viteza = 5;
+    btnBubble = new QPushButton("Bubble Sort", this);
+    btnMergeSort = new QPushButton("Merge Sort", this);
+    btnCompSort = new QPushButton("Comp Sort", this);
+    btnQuickSortRand = new QPushButton("Quick Sort Random", this);
+    btnHeapSort = new QPushButton("Heap Sort", this);
+    btnAmestecare = new QPushButton("Amestecare", this);
+    speed = new QSlider(this);
     btnBubble->setGeometry(0, 0, 120, 40);
     btnMergeSort->setGeometry(120, 0, 120, 40);
+    btnCompSort->setGeometry(240, 0, 120, 40);
+    btnQuickSortRand->setGeometry(360, 0, 120, 40);
+    btnHeapSort->setGeometry(480, 0, 120, 40);
+    btnAmestecare->setGeometry(600, 0, 120, 40);
+    speed->setGeometry(720, 0, 120, 40);
+    speed->setOrientation(Qt::Horizontal);
+    speed->setRange(1, 100);
+    speed->setValue(viteza);
+    connect(speed, &QSlider::valueChanged, this, [this](int val) { viteza = val; });
     connect(btnBubble, &QPushButton::clicked, this, &MainWindow::startBubbleSort);
     connect(btnMergeSort, &QPushButton::clicked, this, &MainWindow::startMergeSort);
+    connect(btnCompSort, &QPushButton::clicked, this, &MainWindow::Sortarea_prin_comparare);
+    connect(btnQuickSortRand, &QPushButton::clicked, this, &MainWindow::startRandomQuickSort);
+    connect(btnHeapSort, &QPushButton::clicked, this, &MainWindow::heapSort);
+    connect(btnAmestecare, &QPushButton::clicked, this, &MainWindow::Amestecare);
     this->showMaximized();
+}
+
+void MainWindow::Amestecare()
+{
+    OprireBtn();
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::shuffle(v.begin(), v.end(), g);
+    update();
+    QCoreApplication::processEvents();
+    PornireBtn();
+}
+
+void MainWindow::Sortarea_prin_comparare()
+{
+    OprireBtn();
+    size_t i, j;
+    for(i=0; i<elemente-1; i++)
+        for(j=i+1; j<elemente; j++)
+            if(v[i] > v[j])
+            {
+                std::swap(v[i], v[j]);
+                update();
+                QCoreApplication::processEvents();
+                QThread::msleep(viteza);
+            }
+    PornireBtn();
 }
 
 MainWindow::~MainWindow()
@@ -24,8 +72,27 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::OprireBtn()
+{
+    btnBubble->setEnabled(false);
+    btnMergeSort->setEnabled(false);
+    btnCompSort->setEnabled(false);
+    btnQuickSortRand->setEnabled(false);
+    btnAmestecare->setEnabled(false);
+}
+
+void MainWindow::PornireBtn()
+{
+    btnBubble->setEnabled(true);
+    btnMergeSort->setEnabled(true);
+    btnCompSort->setEnabled(true);
+    btnQuickSortRand->setEnabled(true);
+    btnAmestecare->setEnabled(true);
+}
+
 void MainWindow::startBubbleSort()
 {
+    OprireBtn();
     bool sortat = false;
     size_t pas = 0;
     while(sortat == false)
@@ -38,10 +105,11 @@ void MainWindow::startBubbleSort()
                 std::swap(v[j], v[j+1]);
                 update();
                 QCoreApplication::processEvents();
-                QThread::msleep(20);
+                QThread::msleep(viteza);
             }
         pas++;
     }
+    PornireBtn();
 }
 
 void MainWindow::Interclasare(size_t st, size_t mij, size_t dr)
@@ -62,7 +130,7 @@ void MainWindow::Interclasare(size_t st, size_t mij, size_t dr)
         else v[k] = aux2[j++];
         update();
         QCoreApplication::processEvents();
-        QThread::msleep(20);
+        QThread::msleep(viteza);
         k++;
     }
     while (i < n)
@@ -70,14 +138,14 @@ void MainWindow::Interclasare(size_t st, size_t mij, size_t dr)
         v[k++] = aux1[i++];
         update();
         QCoreApplication::processEvents();
-        QThread::msleep(20);
+        QThread::msleep(viteza);
     }
     while (j < l)
     {
         v[k++] = aux2[j++];
         update();
         QCoreApplication::processEvents();
-        QThread::msleep(20);
+        QThread::msleep(viteza);
     }
 }
 
@@ -93,14 +161,105 @@ void MainWindow::MergeSort(size_t st, size_t dr)
 void MainWindow::startMergeSort()
 {
     if(elemente == 0) return;
+    OprireBtn();
     MergeSort(0, elemente-1);
+    PornireBtn();
+}
+
+
+void MainWindow::pivot_random(int st, int dr)
+{
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> dist(st, dr);
+    int idx = dist(gen);
+    std::swap(v[idx], v[dr]);
+    update();
+    QCoreApplication::processEvents();
+    QThread::msleep(viteza);
+}
+
+int MainWindow::Aranjare(int st, int dr)
+{
+    pivot_random(st, dr);
+    int pivot = v[dr];
+    int j, i = st;
+    for(j=st; j<dr; j++)
+        if(v[j] <= pivot)
+        {
+            std::swap(v[i], v[j]);
+            update();
+            QCoreApplication::processEvents();
+            QThread::msleep(viteza);
+            i++;
+        }
+    std::swap(v[i], v[dr]);
+    update();
+    QCoreApplication::processEvents();
+    QThread::msleep(viteza);
+    return i;
+}
+
+void MainWindow::QuickSort(int st, int dr)
+{
+    if(st >= dr) return;
+    int pivotIndex = Aranjare(st, dr);
+    QuickSort(st, pivotIndex - 1);
+    QuickSort(pivotIndex + 1, dr);
+}
+
+void MainWindow::startRandomQuickSort()
+{
+    if(elemente == 0) return;
+    OprireBtn();
+    QuickSort(0, (int)elemente - 1);
+    PornireBtn();
+}
+
+
+void MainWindow::make_heap(int n, int i)
+{
+
+    int largest = i;
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+    if (l < n && v[l] > v[largest]) largest = l;
+    if (r < n && v[r] > v[largest]) largest = r;
+    if (largest != i)
+    {
+        std::swap(v[i], v[largest]);
+        update();
+        QCoreApplication::processEvents();
+        QThread::msleep(viteza);
+        make_heap(n, largest);
+    }
+}
+
+void MainWindow::heapSort()
+{
+    if(elemente == 0) return;
+    OprireBtn();
+    int i, n;
+    n = elemente;
+    for (i = n / 2 - 1; i >= 0; i--)
+        make_heap(n, i);
+    for (i = n - 1; i > 0; i--)
+    {
+
+        std::swap(v[0], v[i]);
+        update();
+        QCoreApplication::processEvents();
+        QThread::msleep(viteza);
+        make_heap(i, 0);
+    }
+    PornireBtn();
 }
 
 void MainWindow::paintEvent(QPaintEvent *event)
 {
     QPainter dreptunghi(this);
     QPen pen;
-    pen.setWidth(1);
+    pen.setWidth(0);
     pen.setColor(Qt::black);
     dreptunghi.setBrush(Qt::gray);
     dreptunghi.setPen(pen);
--- mainwindow.h
+++ mainwindow.h
@@ -4,6 +4,8 @@
 #include <QMainWindow>
 #include <QPainter>
 #include <vector>
+#include <QPushButton>
+#include <QSlider>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -16,14 +18,32 @@ class MainWindow : public QMainWindow
     Q_OBJECT
     size_t elemente;
     std::vector<int> v;
+    QPushButton *btnBubble;
+    QPushButton *btnMergeSort;
+    QPushButton *btnCompSort;
+    QPushButton *btnQuickSortRand;
+    QPushButton *btnHeapSort;
+    QPushButton *btnAmestecare;
+    QSlider *speed;
+    int viteza;
     void MergeSort(size_t st, size_t dr);
     void Interclasare(size_t st, size_t mij, size_t dr);
+    int Aranjare(int st, int dr);
+    void QuickSort(int st, int dr);
+    void pivot_random(int st, int dr);
+    void Amestecare();
+    void OprireBtn();
+    void PornireBtn();
+    void make_heap(int n, int i);
 public:
     MainWindow(size_t elemente, const std::vector<int> &v, QWidget *parent = nullptr);
     ~MainWindow();
     virtual void paintEvent(QPaintEvent *event);
     void startBubbleSort();
     void startMergeSort();
+    void Sortarea_prin_comparare();
+    void startRandomQuickSort();
+    void heapSort();
 private:
     Ui::MainWindow *ui;
 };
